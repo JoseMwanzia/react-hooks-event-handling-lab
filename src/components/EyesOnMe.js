38e7d060622805b4e7c1bb7121fa418b850f6871
joseph mwanzia <makamwanzia@gmail.com>
2023-01-20 01:25:13 +0300

    Completed EyesOnMe test

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,22 +1,20 @@
 // Code EyesOnMe Component Here
 
-import React from 'react'
+import React from "react";
+import { ReactDOM } from "react";
 
 function EyesOnMe() {
 
-    function handleFocus(event) {
-        console.log('Good1');
-    }
-
-    function handleBlur(event) {
-        console.log('Hey! Eyes on me!');
-    }
-
   return (
     <div>
-        <button onClick={handleBlur, handleFocus}>Eyes on me</button>
+      <button
+        onBlur={() => console.log("Hey! Eyes on me!")}
+        onFocus={() => console.log("Good!")}
+      >
+        Eyes on me
+      </button>
     </div>
-  )
+  );
 }
 
 export default EyesOnMe;
